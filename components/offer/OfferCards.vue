11c2043c55c4fb5be0acedf2d591acecc765c39b
Hedgehog <dopierala.tb@gmail.com>
2024-09-25 00:12:45 +0200

    f/0.4.0
    -Changes: cards behavior, tiles adjustment

--- components/offer/OfferCards.vue
+++ components/offer/OfferCards.vue
@@ -1,35 +1,48 @@
 <template>
-  <div class="flex justify-between mt-14 flex-col lg:flex-row space-y-5 lg:space-y-0 h-full">
-    <div class="relative shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient">
-      <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT FUNKCJONALNY</h2>
-      <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
-        <li>SPOTKANIE</li>
-        <li>UKŁADY FUNKCJONALNE</li>
-        <li>WIZUALIZACJE</li>
-      </ul>
+  <div class="relative">
+    <div class="flex justify-between mt-14 flex-col lg:flex-row space-y-5 lg:space-y-0 h-full">
+      <!-- Projekt Funkcjonalny -->
+      <div class="lg:absolute shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient z-10 transition-all duration-500">
+        <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT FUNKCJONALNY</h2>
+        <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
+          <li>SPOTKANIE</li>
+          <li>UKŁADY FUNKCJONALNE</li>
+          <li>WIZUALIZACJE</li>
+        </ul>
+      </div>
+
+      <!-- Projekt Koncepcyjny -->
+      <div class="lg:absolute lg:right-[35%] shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient z-10 transition-all duration-500">
+        <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT KONCEPCYJNY</h2>
+        <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
+          <li>SPOTKANIE</li>
+          <li>UKŁADY FUNKCJONALNE</li>
+          <li>WIZUALIZACJE</li>
+          <li>ZESTAWIENIE MATERIAŁÓW</li>
+        </ul>
+      </div>
+
+      <!-- Projekt Kompleksowy -->
+      <div class="lg:absolute lg:right-0 shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient z-10 transition-all duration-500">
+        <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT KOMPLEKSOWY</h2>
+        <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
+          <li>SPOTKANIE</li>
+          <li>INWENTARYZACJA</li>
+          <li>UKŁADY FUNKCJONALNE</li>
+          <li>WIZUALIZACJE</li>
+          <li>RYSUNKI WYKONAWCZE</li>
+          <li>ZESTAWIENIE MATERIAŁÓW</li>
+        </ul>
+      </div>
     </div>
-    <div class="relative shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient">
-      <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT KONCEPCYJNY</h2>
-      <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
-        <li>SPOTKANIE</li>
-        <li>UKŁADY FUNKCJONALNE</li>
-        <li>WIZUALIZACJE</li>
-        <li>ZESTAWIENIE MATERIAŁÓW</li>
-      </ul>
-    </div>
-    <div class="relative shadow-lg p-6 rounded-lg text-white h-fit group bg-gradient">
-      <h2 class="text-3xl mb-0 cursor-pointer transition-all duration-500 group-hover:mb-14">PROJEKT KOMPLEKSOWY</h2>
-      <ul class="space-y-4 text-lg list-disc list-inside max-h-0 overflow-hidden transition-all duration-500 group-hover:max-h-96">
-        <li>SPOTKANIE</li>
-        <li>INWENTARYZACJA</li>
-        <li>UKŁADY FUNKCJONALNE</li>
-        <li>WIZUALIZACJE</li>
-        <li>RYSUNKI WYKONAWCZE</li>
-        <li>ZESTAWIENIE MATERIAŁÓW</li>
-      </ul>
+
+    <!-- Lorem Ipsum Content -->
+    <div class="relative z-0 text-2xl text-white mt-32">
+      Oferuję profesjonalne projekty wnętrz dostosowane do indywidualnych potrzeb klienta, które obejmują różne etapy planowania i realizacji. Moje usługi projektowe skupiają się na tworzeniu funkcjonalnych, estetycznych oraz spersonalizowanych przestrzeni, które odpowiadają oczekiwaniom zarówno w zakresie designu, jak i ergonomii. Zajmujeę się kompleksową obsługą, począwszy od spotkania, przez wizualizacje, aż po szczegółowe opracowania wykonawcze. Każdy projekt realizuję z pełnym zaangażowaniem, oferując fachowe doradztwo oraz dbałość o detale, które przekładają się na harmonijną i przemyślaną przestrzeń.
     </div>
   </div>
 </template>
+
 
 <script setup>
 // Skrypt setup (jeśli jest potrzebny, na razie puste)
